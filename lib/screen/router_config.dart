ccb7a30dd556227a9c58d41d8dc1e758c8f166d2
kataoka-kk <horeizai.1201@gmail.com>
2023-03-18 16:39:54 +0900

    コメント

--- lib/screen/router_config.dart
+++ lib/screen/router_config.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_api_call_test/screen/detail/detail_page.dart';
 import 'package:riverpod_api_call_test/screen/help/help_page.dart';
 import 'package:riverpod_api_call_test/screen/home/home_page.dart';
 
+/// GoRouter
 class MyRouterConfig {
   static final router = GoRouter(
     routes: [
